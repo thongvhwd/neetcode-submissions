610b78b89c70c8d7013673548ec8c5e8f9ac5968
thongvhwd <30382065+thongvhwd@users.noreply.github.com>
2026-04-24 10:43:07 +0700

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.js
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers, target) {
+        //2,3,4,5,6,7,8,9,10,11,12,13,14 target 7
+
+        // let left = 0;
+        // let right = numbers.length - 1;
+        // while (left < right) {
+        //     const numLeft = numbers[left];
+        //     const numRight = numbers[right];
+
+        //     const currentSum = numLeft + numRight;
+        //     if (currentSum > target) {
+        //         right--;
+        //     } else if (currentSum < target) {
+        //         left++;
+        //     } else {
+        //         return [left+1, right+1];
+        //     }
+        // }
+        // return [];
+
+        const map = new Map();
+
+        for (let i = 0; i < numbers.length; i++) {
+            const diff = target - numbers[i];
+            if (map.has(diff)) {
+                return [map.get(diff), i + 1];
+            }
+            map.set(numbers[i], i + 1);
+        }
+        return [];
+    }
+}
